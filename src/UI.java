9cd627c90c8b51d81b931ae4185ee0e5b802c233
CybErr404 <wattsm6@go.stockton.edu>
2025-03-18 19:30:42 -0400

    Removed key object code

--- src/UI.java
+++ src/UI.java
@@ -6,7 +6,6 @@ public class UI {
     GamePanel gp;
     Graphics2D g2;
     Font arial_40, arial_80B;
-    //BufferedImage keyImage;
 
     public boolean messageOn = false;
     public String message = "";
@@ -22,8 +21,6 @@ public class UI {
         this.gp = gp;
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-        //OBJKey key = new OBJKey(gp);
-        //keyImage = key.image;
     }
 
     public void showMessage(String text) {
