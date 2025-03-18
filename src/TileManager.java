52a5c9dba38d10c3512aeb13f01d8958e6eade87
CybErr404 <wattsm6@go.stockton.edu>
2025-03-18 18:12:16 -0400

    Updated TileManager class

--- src/TileManager.java
+++ src/TileManager.java
@@ -17,17 +17,55 @@ public class TileManager {
         tile = new Tile[50];
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
         getTileImage();
-        loadMap("maps/world01.txt");
+        loadMap("maps/worldV2.txt");
     }
 
     public void getTileImage() {
         //PLACEHOLDER TILES
-        setup(0, "grass", false);
-        setup(1, "wall", true);
-        setup(2, "water", true);
-        setup(3, "dirt", false);
-        setup(4, "tree", true);
-        setup(5, "sand", false);
+        setup(0, "grass00", false);
+        setup(1, "grass00", false);
+        setup(2, "grass00", false);
+        setup(3, "grass00", false);
+        setup(4, "grass00", false);
+        setup(5, "grass00", false);
+        setup(6, "grass00", false);
+        setup(7, "grass00", false);
+        setup(8, "grass00", false);
+        setup(9, "grass00", false);
+
+        //USED TILES
+        setup(10, "grass00", false);
+        setup(11, "grass01", false);
+        setup(12, "water00", false);
+        setup(13, "water01", false);
+        setup(14, "water02", false);
+        setup(15, "water03", false);
+        setup(16, "water04", false);
+        setup(17, "water05", false);
+        setup(18, "water06", false);
+        setup(19, "water07", false);
+        setup(20, "water08", false);
+        setup(21, "water09", false);
+        setup(22, "water10", false);
+        setup(23, "water11", false);
+        setup(24, "water12", false);
+        setup(25, "water13", false);
+        setup(26, "road00", false);
+        setup(27, "road01", false);
+        setup(28, "road02", false);
+        setup(29, "road03", false);
+        setup(30, "road04", false);
+        setup(31, "road05", false);
+        setup(32, "road06", false);
+        setup(33, "road07", false);
+        setup(34, "road08", false);
+        setup(35, "road09", false);
+        setup(36, "road10", false);
+        setup(37, "road11", false);
+        setup(38, "road12", false);
+        setup(39, "dirt", false);
+        setup(40, "wall", true);
+        setup(41, "tree", true);
     }
 
     public void setup(int index, String imageName, boolean collision) {
@@ -35,7 +73,7 @@ public class TileManager {
 
         try {
             tile[index] = new Tile();
-            tile[index].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Tiles/OldTiles/" +
+            tile[index].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("Tiles/" +
                     imageName + ".png"));
             tile[index].image = utilityTool.scaleImage(tile[index].image, gp.tileSize, gp.tileSize);
             tile[index].collision = collision;
@@ -89,9 +127,9 @@ public class TileManager {
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
             if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
-                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
-                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
-                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+                    worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                    worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                    worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 
                 g2.drawImage(tile[tileNum].image, screenX, screenY, null);
 
